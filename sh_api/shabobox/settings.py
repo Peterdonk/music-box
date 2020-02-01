2133b4dbc8c993e78566d126ca182f56220d1566
dynacode99 <wahab.awudu@mybestquote.com>
2020-02-01 11:02:38 +0000

    edited workspace setup

--- sh_api/shabobox/settings.py
+++ sh_api/shabobox/settings.py
@@ -1,9 +1,13 @@
 import os
 import datetime
 import dj_database_url
+import environ
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+env = environ.Env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
--- sh_api/shabobox/urls.py
+++ sh_api/shabobox/urls.py
@@ -18,20 +18,21 @@ router.register(r'users', UserViewSet, basename='users')
 
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^admin/', admin.site.urls),
+    # url(r'^', include('django.contrib.auth.urls')),
     url(r'^accounts/', include('allauth.urls')),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^docs/', include('rest_framework_docs.urls')),
-    url(r'^rest-auth/registration/account-email-verification-sent/', null_view,
+
+    # auth urls
+    url(r'^auth/registration/account-email-verification-sent/', null_view,
         name='account_email_verification_sent'),
-    url(r'^rest-auth/registration/account-confirm-email/', null_view, name='account_confirm_email'),
+    url(r'^auth/registration/account-confirm-email/', null_view, name='account_confirm_email'),
     url(
         r'^password-reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         null_view, name='password_reset_confirm'),
-    url(r'^api/v1/rest-auth/', include('rest_auth.urls')),
+    url(r'^api/v1/auth/', include('rest_auth.urls')),
     url(r'^verify-email/(?P<key>\w+)/$', confirm_email, name="account_confirm_email"),
-    url(r'^api/v1/rest-auth/registration/', include('rest_auth.registration.urls')),
-    url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api/v1/auth/registration/', include('rest_auth.registration.urls')),
+    url(r'^auth/v1/api-token-auth/', obtain_jwt_token),
     url(r'^api-token-refresh/', refresh_jwt_token),
+    url(r'^docs/', include('rest_framework_docs.urls')),
+    url(r'^api/v1/', include(router.urls), name='home'),
 ]
